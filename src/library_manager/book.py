80781bf8e28cb86e694f4b1f29073a035d74f367
Darci Ramson Junior <rdarcijunior@gmail.com>
2025-05-01 19:51:33 -0300

    feat: implement basic version of Book class

---
+++ src/library_manager/book.py
@@ -0,0 +1,38 @@
+# src/library_manager/book.py
+
+class Book:
+    """Represents a single book in a library."""
+
+    def __init__(self, title: str, author: str, isbn: str):
+        """
+        Initializes a new Book object.
+        
+        Args:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            isbn (str): The International Standard Book Number.
+        """
+        # --- Attributes ---
+        # These are public attributes, acessible directly
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.is_read = False # By default when a book is added it wasn't read yet
+
+    # Methods
+    def mark_as_read(self):
+        """Marks the book as read."""
+        self.is_read = True
+        print(f"Book '{self.title}' marked as read.")
+
+    def mark_as_unread(self):
+        """Marks the book as unread."""
+        self.is_read = False
+        print(f"Book '{self.title}' marked as unread.")
+
+    def __str__(self) -> str:
+        """
+        Returns a user-friendly string representation of the book.
+        """
+        status = "Read" if self.is_read else "Unread"
+        return f"'{self.title}' by '{self.author}' (ISBN: {self.isbn}) - Status: {status}"
